5e855b60216255e73df92910c84702f2ca84b342
ElijahQuinones <118852979+ElijahQuinones@users.noreply.github.com>
2022-11-22 16:02:16 -0500

    Update Simple_text_file_parser.py

--- Simple_text_file_parser.py
+++ Simple_text_file_parser.py
@@ -3,12 +3,22 @@ justcve = open("jcve.txt",'w')
 
 for line in cvefile:
 	try:
-		cve, junk,junktwo,platform = line.split('\t')
-		justcve.write(cve + '\t'+ platform+ '\n')
+		cve, junk,= line.split('\t')
+		justcve.write(cve + '\n')
 	except:
-		print("file does not contain 4 values separated by tabs on this line ")
+		print("file does not contain 2 values separated by tabs on this line ")
 		try:
-			cve, junk, junktwo, platform,junkthree = line.split('\t')
-			justcve.write(cve + '\t' + platform + '\n')
+			cve, junk,junktwo = line.split('\t')
+			justcve.write(cve + '\n')
 		except:
-			print("nor does it contain 5 values separated by tabs on this line")
+			print("file does not contain 3 values separated by tabs on this line ")
+			try:
+				cve, junk,junktwo,platform = line.split('\t')
+				justcve.write(cve + '\t'+ platform+ '\n')
+			except:
+				print("file does not contain 4 values separated by tabs on this line ")
+				try:
+					cve, junk, junktwo, platform,junkthree = line.split('\t')
+					justcve.write(cve + '\t' + platform + '\n')
+				except:
+					print("nor does it contain 5 values separated by tabs on this line")
